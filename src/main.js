bf73669f405fc8e9a4e36d5261f80c5a142c04e4
tunt-mor <vunt@mor.com.vn>
2022-08-24 15:37:31 +0700

    add library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "@/core/router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import BootstrapVue, { IconsPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+Vue.use(IconsPlugin);
+Vue.use(Antd);
+Vue.use(BootstrapVue);
 
 new Vue({
   render: (h) => h(App),
