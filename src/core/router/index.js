c59bb5bf67debafbd59632cc0949c554091602da
tunt-mor <vunt@mor.com.vn>
2022-08-24 15:27:36 +0700

    add-router-vue

---
+++ src/core/router/index.js
@@ -0,0 +1,14 @@
+﻿import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+let router = new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/test",
+      component: () => import("@/view/dailyReport/DailyReport.vue"),
+      meta: {},
+    },
+  ],
+});
+export default router;
